847d9f080a977878c5e7383d15b65b3da7ac3bd9
olfasahnoun01 <olfasahnoun01@gmail.com>
2026-04-14 09:44:36 +0100

    mmmm-

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://rnujsdxbkndvppjqjkdu.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_osE5pQrW9xN0djc5XUTbdQ_vrxcqX_c";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
